fb09f15946b50f0a536a7fe217f68cba0b279718
dankato <apearmor@gmail.com>
2017-05-18 11:52:37 -0700

    updates to js, names, api function & state mods

--- app.js
+++ app.js
@@ -1,29 +1,55 @@
 const appState = {
-  
+  beginning: [], // user input 1
+  middle: [], // user input 2
+  end: [], // user input 3
+  //embed_url: [stick guys here], // https://giphy.com/embed/U7Lvtcuqh4WZy
 };
 
-
 /////STATE MODS/////
-// http://api.giphy.com/v1/gifs/search?q=funny+cat&api_key=dc6zaTOxFJmzC   
+// http://api.giphy.com/v1/gifs/search?q=funny+cat&api_key=dc6zaTOxFJmzC
 
 let giphyURL='http://api.giphy.com/v1/gifs/search';
-
+// Giphy API Functions
 function getDataFromApi(searchTerm, callback) {
   var query = {
     part: 'slug',
-    limit: 1,
+    limit: 9,
     sort: 'relevant',
     q: searchTerm,
     api_key: 'dc6zaTOxFJmzC'
   };
   $.getJSON(giphyURL, query, callback);
 }
+// Beginning Story
+function fillinBeginning (state, userElement) {
+  if (userElement === null) {
+      state.beginning.push('');
+  } else {
+      state.beginning.push(userElement);
+  }
+};
+// Middle Story
+function fillinMiddle (state, userElement) {
+  if (userElement === null) {
+      state.middle.push('');
+  } else {
+      state.middle.push(userElement);
+  }
+};
+// End Story
+function fillinEnd (state, userElement) {
+  if (userElement === null) {
+      state.end.push('');
+  } else {
+      state.end.push(userElement);
+  }
+};
+// Fill the URL with my element
+function fillURL (state, urlElement) {
+  state.embed_url.push(urlElement);
+};
 
 
-function displayGiphySearchData(data) {
-  console.log(data);
-//   document.write(`<a href="${data.data[0].bitly_gif_url}">"Link"</a>`);
-}
 
 getDataFromApi('sleep', displayGiphySearchData);
 // getDataFromApi('I got up this morning.', displayGiphySearchData);
@@ -32,7 +58,10 @@ getDataFromApi('sleep', displayGiphySearchData);
 ////////RENDERING FUNCTIONS/////////////
 
 
+function displayGiphySearchData(data) {
+  console.log(data);
+//   document.write(`<a href="${data.data[0].bitly_gif_url}">"Link"</a>`);
+}
 
 
 //////////////EVENT LISTENERS////////////////
-
